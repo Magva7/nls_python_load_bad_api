bf38c125eb6434620f2426d075ca6954d7fdabc7
magva7 <magva7@gmail.com>
2023-09-16 11:00:23 +0600

    Скелет

--- django_project_nls_load_bad_api/wsgi.py
+++ django_project_nls_load_bad_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_edition_nls_load_bad_api project.
+WSGI config for django_project_nls_load_bad_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_edition_nls_load_bad_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project_nls_load_bad_api.settings')
 
 application = get_wsgi_application()
