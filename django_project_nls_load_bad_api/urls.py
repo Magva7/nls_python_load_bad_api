273fcd82b8227f54cb0cb86b1e40ddd719c70d08
magva7 <magva7@gmail.com>
2023-09-16 12:01:37 +0600

    api работает, добавил проверку на эксепшены

--- django_project_nls_load_bad_api/urls.py
+++ django_project_nls_load_bad_api/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp1.views import index_page
+from myapp1.views import sync_data_with_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', index_page), # при обращении к главной странице должна запускаться функция index_page
-    # path('about', about_page)
+     # при обращении к главной странице должна запускаться функция index_page
+	path('', index_page),
+    path('sync_data/', sync_data_with_api, name='sync_data'),
 ]
--- myapp1/models.py
+++ myapp1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class MyData(models.Model):
+    # Поле, в котором храним primary key
+    primary_key_field = models.CharField(max_length=255, unique=True)
+    # Поля, в которых храним строковые данные
+    field1 = models.CharField(max_length=255)
+    field2 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.primary_key_field
